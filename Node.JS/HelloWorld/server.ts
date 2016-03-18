29e9627c44468313897f259c33a55d68e55a7e09
Thomas Coville <tcoville@landr.com>
2016-03-18 18:41:02 -0400

    hello world

---
+++ Node.JS/HelloWorld/server.ts
@@ -0,0 +1,16 @@
+﻿import http = require('http');
+var port = process.env.port || 1337
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
+var ws = require('ws');
+
+var wss = new ws.Server({ port: 8080 });
+wss.on('connection', function (connection) {
+    connection.on('message', function (message) {
+        connection.send(message.toUpperCase());
+    });
+    connection.send('Hello!');
+});
